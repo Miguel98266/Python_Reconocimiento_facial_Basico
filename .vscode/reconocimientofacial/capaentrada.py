3ffa1d73e7b58f2e0ef06120e133f6ccdad9e06c
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2021-08-05 22:19:33 -0500

    v1

--- .vscode/reconocimientofacial/capaentrada.py
+++ .vscode/reconocimientofacial/capaentrada.py
@@ -1,10 +1,22 @@
 import cv2 as cv
-import 
+import os 
+
+modelo='FotosMiguel'
+ruta1='M:/Python_Reconocimiento_facial/reconocimientofacial1'
+rutacompleta=ruta1+ '/' + modelo
+if not os.path.exists(rutacompleta):
+    os.makedirs(rutacompleta)
+
+
 ruidos=cv.CascadeClassifier('M:\Python_Reconocimiento_facial\entrenamientos opencv ruidos\opencv-master\opencv-master\data\haarcascades\haarcascade_frontalface_default.xml')
 camara=cv.VideoCapture(0)
+id=0
 while True:
-    _,captura=camara.read()
+    respuesta,captura=camara.read()
+    if respuesta==False:
+        break
     grises=cv.cvtColor(captura,cv.COLOR_BGR2GRAY)
+    idcaptura=captura.copy()
     cara=ruidos.detectMultiScale(grises,1.3,5)
     for(x,y,e1,e2) in cara:
         cv.rectangle(captura,(x,y),(x+e1,y+e2),(255,0,0),2)
