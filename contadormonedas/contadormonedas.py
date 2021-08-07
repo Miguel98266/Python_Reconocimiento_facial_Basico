780ab65a6bd72a9b7ba04552fe90b42bf4ac5056
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2021-08-07 16:42:53 -0500

    v1

--- contadormonedas/contadormonedas.py
+++ contadormonedas/contadormonedas.py
@@ -3,7 +3,7 @@ import numpy as np
 varGauss=5
 valorKernel=7
 #original=cv2.imread('monedassoles.jpg')
-original=cv2.imread('monedas.jpg')
+original=cv2.imread('M:/Python_Reconocimiento_facial/contadormonedas/monedas.jpg')
 gris=cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
 #Primera eliminacion de ruidos
 gauss=cv2.GaussianBlur(gris,(varGauss,varGauss),0)
@@ -15,6 +15,7 @@ cierre= cv2.morphologyEx(canny,cv2.MORPH_CLOSE,kernel)
 contornos,jerarquia=cv2.findContours(cierre.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
 print("Monedas encontradas: {}".format(len(contornos)))
+cv2.imshow('Original',original)
 cv2.drawContours(original,contornos,-1,(0,0,225),2)
 #Mostrar resultado
 cv2.imshow('Grises',gris)
