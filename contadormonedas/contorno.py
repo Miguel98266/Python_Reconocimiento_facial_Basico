aeb36ecde67b2b59ee58756f2fd1a3c1a5d17be1
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2021-08-07 15:55:51 -0500

    v1

--- contadormonedas/contorno.py
+++ contadormonedas/contorno.py
@@ -1,5 +1,5 @@
 from cv2 import cv2
-imagen=cv2.imread('contorno.jpg')
+imagen=cv2.imread('M:/Python_Reconocimiento_facial/contadormonedas/contorno.jpg')
 grises=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
 tipoUmbral,umbral=cv2.threshold(grises,100,255,cv2.THRESH_BINARY)
 contorno,jerarquia=cv2.findContours(umbral,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
